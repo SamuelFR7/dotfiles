466f468cf0a8219cfa7fa6ffe96bc67b443addc9
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2024-08-29 15:00:12 -0300

    feat: zellij config

--- dot_config/nvim/after/plugin/treesitter.lua
+++ dot_config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "c", "javascript", "typescript", "rust", "lua", "vim", "vimdoc" },
+    -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+    ensure_installed = { "c", "javascript", "typescript", "rust", "lua", "vim", "vimdoc" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
