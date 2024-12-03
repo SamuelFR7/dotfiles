6e526b55612dbf04bebc6a321205963052c4cd28
Samuel Ferreira <samuel.ferreira@ipeweb.com.br>
2024-12-03 16:04:29 -0300

    some changes in nvim config

--- dot_config/nvim/lua/samuelfr/lazy/conform.lua
+++ dot_config/nvim/lua/samuelfr/lazy/conform.lua
@@ -1,27 +1,24 @@
 return {
 	"stevearc/conform.nvim",
-	config = function()
-		local conform = require("conform")
-		conform.setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				javascript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescript = { "prettier" },
-				typescriptreact = { "prettier" },
-				json = { "prettier" },
-				php = { "php-cs-fixer" },
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			javascript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescript = { "prettier" },
+			typescriptreact = { "prettier" },
+			json = { "prettier" },
+			php = { "php-cs-fixer" },
+		},
+		formatters = {
+			["php-cs-fixer"] = {
+				command = "php-cs-fixer",
+				args = function(_, ctx)
+					return { "fix", "--quiet", "--using-cache=no", "--config", ".php-cs-fixer.php", ctx.filename }
+				end,
+				stdin = false,
+				timeout = 10000,
 			},
-			formatters = {
-				["php-cs-fixer"] = {
-					command = "php-cs-fixer",
-					args = function(_, ctx)
-						return { "fix", "--quiet", "--using-cache=no", "--config", ".php-cs-fixer.php", ctx.filename }
-					end,
-					stdin = false,
-					timeout = 10000,
-				},
-			},
-		})
-	end,
+		},
+	},
 }
--- dot_config/nvim/lua/samuelfr/set.lua
+++ dot_config/nvim/lua/samuelfr/set.lua
@@ -18,3 +18,9 @@ vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
+
+vim.fn.sign_define("DapBreakpoint", { text = " ", texthl = "DiagnosticError", linehl = "", numhl = "" })
+vim.fn.sign_define("DapBreakpointCondition", { text = " ", texthl = "DiagnosticWarn", linehl = "", numhl = "" })
+vim.fn.sign_define("DapLogPoint", { text = ".>", texthl = "DiagnosticInfo", linehl = "", numhl = "" })
+vim.fn.sign_define("DapStopped", { text = "󰁕 ", texthl = "DiagnosticWarn", linehl = "DapStoppedLine", numhl = "" })
+vim.fn.sign_define("DapBreakpointRejected", { text = " ", texthl = "DiagnosticError", linehl = "", numhl = "" })
