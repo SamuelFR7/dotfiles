535e7c0069f303b6e4ac6a62692eddfd3a8ebc20
Samuel Ferreira <samuel.ferreira@ipeweb.com.br>
2024-12-18 14:20:23 -0300

    Update .config/nvim/lua/plugins/conform.lua

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -26,7 +26,7 @@ return {
 			lsp_format = "fallback",
 		},
 		format_on_save = function(bufnr)
-			local ignore_filetypes = { "php", "typescript" }
+			local ignore_filetypes = { "php" }
 			if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
 				return
 			end
